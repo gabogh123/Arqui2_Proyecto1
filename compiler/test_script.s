884468a90162bb051d126c7347e048a48715669c
Gabriel González <gabogh99@gmail.com>
2024-04-10 17:11:33 -0600

    feat(assembly test): Add instruction

--- compiler/test_script.s
+++ compiler/test_script.s
@@ -2,12 +2,13 @@ somi $e0, $zero, 1 # Guarda un 1 a $e0
 
 somi $e1, $e0, 4 # Suma 1 + 4 = 5
 
-somi $e2, $zero, 8 # Suma 0 + 8 = 8
+som $e2, $e1, $e0 # Suma 5+1 = 6
 
-somi $e3, $e2, 2 # Suma 8 + 2 = 10
+som $e3, $e2, $e1 # Suma 6+5 = 11
 
-somi $e1, $e1, 3 # Suma 5 + 3  
+som $e4, $e3, $e3 # Suma 11+11 = 22 
 
+som $e1, $e1, $e4 # Suma 5+22 = 27
 
 
 
