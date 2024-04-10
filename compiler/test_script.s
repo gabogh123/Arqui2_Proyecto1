e31949bd8d26a6e80231b8011c6899d30f895752
Gabriel González <gabogh99@gmail.com>
2024-04-10 14:36:47 -0600

    feat(assembly_test): Addi testing

---
+++ compiler/test_script.s
@@ -0,0 +1,13 @@
+somi $e0, $zero, 1 # Guarda un 1 a $e0
+
+somi $e1, $e0, 4 # Suma 1 + 4 = 5
+
+somi $e2, $zero, 8 # Suma 0 + 8 = 8
+
+somi $e3, $e2, 2 # Suma 8 + 2 = 10
+
+somi $e1, $e1, 3 # Suma 5 + 3  
+
+
+
+
